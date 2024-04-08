7e12a1c6bcfcb416500894b9b4e04732b186ba4c
sou31415 <tbc3141@icloud.com>
2024-04-08 22:20:01 +0900

    first commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,19 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//.invoke_handler(tauri::generate_handler![greet])
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+    let tray = SystemTray::new().with_menu(tray_menu);
+    tauri::Builder::default()
+        .system_tray(tray)
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
